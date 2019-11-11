bdfe28ca6e8c4e13c210c5fc8c60e3bf14c4d182
Habib <habibalizada@gmail.com>
2019-11-11 09:33:20 -0600

    Seeting up for Feature Routing

--- APM-Start/readme.md
+++ APM-Start/readme.md
@@ -3,6 +3,7 @@
 ## Table of Contents
 
 [Feature Modules](#Feature-Modules)\
+[Setting up for Feature Routing](#Setting-up-for-Feature-Routing)\
 
 ---
 
@@ -15,5 +16,11 @@ Using feature modeles allows us to consolidate the components for a specific app
 ![Feature Modules](./src/assets/images/02/03.jpg "")
 By the end of this course module we configure basic routing for 'Log In' and 'Product List' features.
 ![Feature Modules](./src/assets/images/02/04.jpg "")
+
+---
+
+### Setting up for Feature Routing
+
+![Feature Modules](./src/assets/images/02/05.jpg "")
 
 ---
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -5,10 +5,14 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router'; // 1.a. Import Router
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([ // 1.b. Add RouterModule here since it is feature rout we use .forChiled()
+      { path: 'products', component: ProductListComponent }, // 2. Configure routes
+    ])
   ],
   declarations: [
     ProductListComponent,
